6ca3ab20434456ae5ac16b46548931fe3f8ef793
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2024-03-06 20:11:21 +0600

    Task 4: Report the results in the CSV and JSON output format. The tests are passing as described:
    $ python3 -m unittest
    .................................................................
    -----------------------------------------------------------------
    Ran 73 tests in 1.077s
    
    OK

--- write.py
+++ write.py
@@ -12,6 +12,7 @@ You'll edit this file in Part 4.
 """
 import csv
 import json
+from helpers import datetime_to_str
 
 
 def write_to_csv(results, filename):
@@ -24,11 +25,26 @@ def write_to_csv(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
+
     fieldnames = (
         'datetime_utc', 'distance_au', 'velocity_km_s',
         'designation', 'name', 'diameter_km', 'potentially_hazardous'
     )
-    # TODO: Write the results to a CSV file, following the specification in the instructions.
+
+    with open(filename, 'w', newline='') as outfile:
+        writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for approach in results:
+            row = {
+                'datetime_utc': datetime_to_str(approach.time),
+                'distance_au': approach.distance,
+                'velocity_km_s': approach.velocity,
+                'designation': approach.neo.designation,
+                'name': approach.neo.name if approach.neo.name else '',
+                'diameter_km': approach.neo.diameter if approach.neo.diameter else 'nan',
+                'potentially_hazardous': 'True' if approach.neo.hazardous else 'False'
+            }
+            writer.writerow(row)
 
 
 def write_to_json(results, filename):
@@ -42,4 +58,20 @@ def write_to_json(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    # TODO: Write the results to a JSON file, following the specification in the instructions.
+    data = []
+    for approach in results:
+        entry = {
+            'datetime_utc': datetime_to_str(approach.time),
+            'distance_au': approach.distance,
+            'velocity_km_s': approach.velocity,
+            'neo': {
+                'designation': approach.neo.designation,
+                'name': approach.neo.name if approach.neo.name else '',
+                'diameter_km': approach.neo.diameter if approach.neo.diameter else float('nan'),
+                'potentially_hazardous': approach.neo.hazardous
+            }
+        }
+        data.append(entry)
+
+    with open(filename, 'w') as outfile:
+        json.dump(data, outfile, indent=2)
