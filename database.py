c73961c95f9bafa4b95228ed79ecb59a0c45813d
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2024-03-06 19:52:45 +0600

    Task 3(B):Query the database of close approaches using user-specified criteria for the Udacity-PyND

--- database.py
+++ database.py
@@ -78,6 +78,6 @@ class NEODatabase:
         :param filters: A collection of filters capturing user-specified criteria.
         :return: A stream of matching `CloseApproach` objects.
         """
-        # TODO: Generate `CloseApproach` objects that match all of the filters.
         for approach in self._approaches:
-            yield approach
+            if all(f(approach) for f in filters):
+                yield approach
